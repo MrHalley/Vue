326a9811aebc7350faf6626d77ceca4915cbfd14
Mr.Du <1165449791@qq.com>
2020-10-31 11:49:10 +0800

    Babel的使用
    Babel是一个广泛使用的转码器，可以将ES6代码转为ES5代码，从而在现有环境执行执行。
    这意味着，你可以现在就用 ES6 编写程序，而不用担心现有环境是否支持。

---
+++ babelDemo/es5.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var a = 1,
+    b = 2,
+    c = 3;
+
+
+var input = [1, 2, 3];
+input = input.map(function (item) {
+  return item + 1;
+});
+console.log(input);
---
+++ babelDemo/es6.js
@@ -0,0 +1,5 @@
+let [a,b,c] = [1,2,3];
+
+let input = [1,2,3]
+input = input.map(item => item + 1)
+console.log(input);
